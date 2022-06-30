96e4f8b524ebec5e14b30f314adf00e10498e0e7
Kenji <ijnekmail@gmail.com>
2022-06-30 16:33:15 +0200

    Clean slate, removed all contents from GameBoard Panel.

--- src/azul/team12/view/AzulView.java
+++ src/azul/team12/view/AzulView.java
@@ -1,43 +1,44 @@
 package azul.team12.view;
 
 import azul.team12.view.board.GameBoard;
-import java.awt.CardLayout;
-import java.awt.Dimension;
-import java.awt.GridBagLayout;
-import java.awt.HeadlessException;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
+import azul.team12.controller.Controller;
+
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Objects;
+import javax.swing.*;
 
 public class AzulView extends JFrame {
 
   private static final long serialVersionUID = 7526472295622776147L;
+  private final Controller controller;
+  private final int WIDTH_OF_WINDOW = 1200;
+  private final int HEIGHT_OF_WINDOW = 800;
 
   private static final String LOGIN_CARD = "login";
   private static final String HSM_CARD = "hotseatmode";
   private static final String NETWORK_CARD = "networkmode";
   private static final String GAMEBOARD_CARD = "gameboard";
-  private final GridBagLayout gbl;
   private CardLayout layout;
   private JTextArea inputNameArea;
+  private JTextArea playerTwoNameArea;
   private JButton hsmButton;
   private JButton networkButton;
   private JButton addPlayerButton;
   private JButton playButton;
-  private int numberOfPlayers;
 
-  public AzulView() throws HeadlessException {
+  public AzulView(Controller controller) throws HeadlessException {
+    this.controller = controller;
+
     setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    gbl = new GridBagLayout();
-    //setLayout(new GridBagLayout());
-    setMinimumSize(new Dimension(1200, 800));
-    setExtendedState(JFrame.MAXIMIZED_BOTH);
+    setLayout(new BorderLayout());
+    setSize(new Dimension(WIDTH_OF_WINDOW, HEIGHT_OF_WINDOW));
 
     initializeWidgets();
     addEventListeners();
     createView();
+
   }
 
   private void initializeWidgets() {
@@ -46,22 +47,52 @@ public class AzulView extends JFrame {
     hsmButton = new JButton("Hot Seat Mode");
     networkButton = new JButton("Network Mode");
     addPlayerButton = new JButton("+ Add Player");
+    addPlayerButton.setEnabled(false);
     playButton = new JButton("Play");
+    playButton.setEnabled(false);
+
+    //Textfields
+    inputNameArea = new JTextArea(1,20);
+    inputNameArea.setLineWrap(true);
+    inputNameArea.setWrapStyleWord(true);
+    inputNameArea.setBorder(new JTextField().getBorder());
+
+    playerTwoNameArea = new JTextArea(1,20);
+    playerTwoNameArea.setLineWrap(true);
+    playerTwoNameArea.setWrapStyleWord(true);
+    playerTwoNameArea.setBorder(new JTextField().getBorder());
+
     //Labels
 
   }
 
   private void addEventListeners() {
-    //TODO: swap lambda expressions with controller functions
     hsmButton.addActionListener(event -> showHSMCard());
     networkButton.addActionListener(event -> showNetworkCard());
     playButton.addActionListener(event -> showGameBoard());
+    addTextAreaListener(inputNameArea);
+    addTextAreaListener(playerTwoNameArea);
+  }
 
-    /*
-    addPlayerButton.addActionListener(
-            //TODO: if numberOfPlayers < 5, add another inputNameArea, else disable button
-    );
-    */
+  private void addTextAreaListener(JTextArea jTextArea) {
+    jTextArea.addKeyListener(new KeyAdapter() {
+      @Override
+      public void keyPressed(KeyEvent event) {
+        if (event.getKeyCode() != KeyEvent.VK_ENTER) {
+          return;
+        }
+        event.consume();
+        Objects.requireNonNull(jTextArea);
+
+        String nick = jTextArea.getText();
+        controller.addPlayer(nick);
+        jTextArea.setText(nick);
+        jTextArea.setEnabled(false);
+        if (controller.getPlayerNamesList().size() > 1) {
+          playButton.setEnabled(true);
+        }
+      }
+    });
   }
 
   private void createView() {
@@ -75,17 +106,19 @@ public class AzulView extends JFrame {
     login.add(networkButton);
 
     JPanel hsmPanel = new JPanel();
-    inputNameArea = new JTextArea();
     add(hsmPanel, HSM_CARD);
+    hsmPanel.add(new JLabel("Please enter usernames and hit ENTER"));
     hsmPanel.add(new JLabel("Player 1: "));
     hsmPanel.add(inputNameArea);
+    hsmPanel.add(new JLabel("Player 2: "));
+    hsmPanel.add(playerTwoNameArea);
     hsmPanel.add(addPlayerButton);
     hsmPanel.add(playButton);
-
     //controller.startGameBoard
   }
 
-  //TODO: add propertyChange function
+  //TODO: add AzulView extends PropertyChange
+  // override dispose(), propertyChange() methods.
   private void showHSMCard() {
     showCard(HSM_CARD);
   }
@@ -95,10 +128,13 @@ public class AzulView extends JFrame {
   }
 
   private void showGameBoard() {
+
+    controller.startGame();
+
     JPanel gameBoardPanel = new JPanel();
     add(gameBoardPanel, GAMEBOARD_CARD);
     //GameBoard gameBoard = new GameBoard(gbl);
-    GameBoard gameBoard = new GameBoard(this.getWidth(), this.getHeight());
+    GameBoard gameBoard = new GameBoard();
 
     gameBoardPanel.add(gameBoard);
     showCard(GAMEBOARD_CARD);
--- src/azul/team12/view/board/GameBoard.java
+++ src/azul/team12/view/board/GameBoard.java
@@ -1,63 +1,9 @@
 package azul.team12.view.board;
 
-import azul.team12.view.board.playerBoard.Plates;
-import azul.team12.view.board.playerBoard.PlayerBoard;
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import javax.swing.BoxLayout;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 public class GameBoard extends JPanel {
-
-  private static final long serialVersionUID = 7526472295622776147L;
-  //List<PlayerBoard> playerBoardList = new ArrayList<>();
-  PlayerBoard currentPlayerBoard;
-  //  List<Plate> plateList;
-  private int numberOfPlayers = 4;// TODO->lang of playerList
-  private int numberOfPlates;
-
-  private int panelDrawWidth;
-  private int panelDrawHeight;
-
-  public GameBoard(int width, int height) {
-
-    panelDrawWidth = width;
-    panelDrawHeight = height;
-    setPreferredSize(new Dimension(1200, 800));
-    setLayout(new BorderLayout());
-    // setBackground(new Color(110,90,120));
-    createLeft();
-    createCenter();
-  }
-
-  private void createCenter() {
-    JPanel center = new JPanel();
-    center.setLayout(new BorderLayout());
-    numberOfPlates = numberOfPlayers * 2 + 1;
-//    plateList = new ArrayList<>();
-//    Plates plates = new Plates(numberOfPlates, this.plateList);
-    Plates plates = new Plates(numberOfPlates);
-    center.add(plates, BorderLayout.CENTER);
-
-    currentPlayerBoard = new PlayerBoard(400, 300);
-    currentPlayerBoard.setBorder(new EmptyBorder(0, 80, 20, 80));
-
-    center.add(currentPlayerBoard, BorderLayout.SOUTH);
-    add(center, BorderLayout.CENTER);
-  }
-
-  private void createLeft() {
-    JPanel othersPanel = new JPanel();
-    othersPanel.setLayout(new BoxLayout(othersPanel, BoxLayout.Y_AXIS));
-
-    for (int i = 0; i < numberOfPlayers - 1; i++) {
-      PlayerBoard playerBoard = new PlayerBoard();
-
-      //playerBoardList.add(playerBoard);
-      othersPanel.add(playerBoard);
-    }
-    add(othersPanel, BorderLayout.WEST);
+  public GameBoard() {
   }
 
 }
