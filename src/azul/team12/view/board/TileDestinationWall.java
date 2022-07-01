32bd7c8ed9257303c1925f3d830360fae3cf8d8b
Iurii Irkha <irkhai@cip.ifi.lmu.de>
2022-07-01 12:07:07 +0200

    Tiles can be moved

---
+++ src/azul/team12/view/board/TileDestinationWall.java
@@ -0,0 +1,72 @@
+package azul.team12.view.board;
+
+
+import azul.team12.view.listeners.TileClickListener;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TileDestinationWall extends JPanel {
+
+  private final int cell;
+  private final int row;
+  private final int cellSize;
+
+  private ImageIcon icon;
+  private JLabel label;
+
+  public TileDestinationWall(int cell, int row, int cellSize, TileClickListener tileClickListener,
+      ImageIcon icon) {
+    setLayout(new GridLayout(1, 1));
+    this.cell = cell;
+    this.row = row;
+    this.cellSize = cellSize;
+    this.icon = icon;
+    label = icon != null ? new JLabel(icon) : new JLabel("icon");
+    add(label);
+    setPreferredSize(new Dimension(cellSize, cellSize));
+    setMaximumSize(new Dimension(cellSize, cellSize));
+    setMinimumSize(new Dimension(cellSize, cellSize));
+    setBackground(Color.WHITE);
+    setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+    addMouseListener(tileClickListener);
+  }
+
+  @Override
+  public Dimension getPreferredSize() {
+    return new Dimension(cellSize + 2, cellSize + 2);
+  }
+
+  public int getCell() {
+    return cell;
+  }
+
+  public int getRow() {
+    return row;
+  }
+
+  public int getCellSize() {
+    return cellSize;
+  }
+
+  public ImageIcon getIcon() {
+    return icon;
+  }
+
+  public void setIcon(ImageIcon icon) {
+    this.icon = icon;
+  }
+
+  public JLabel getLabel() {
+    return label;
+  }
+
+  public void setLabel(JLabel label) {
+    this.label = label;
+  }
+}
---
+++ src/azul/team12/view/listeners/IDestinationTileListener.java
@@ -0,0 +1,8 @@
+package azul.team12.view.listeners;
+
+import azul.team12.view.board.TileDestination;
+
+public interface IDestinationTileListener {
+
+  void onDestinationTileClick(TileDestination tileDestination);
+}
