63a773741825aebbe30a28f0ee2683f44a1f56a2
Nils Sirrenberg <sirrenberg@cip.ifi.lmu.de>
2022-07-01 14:36:08 +0200

    implemented AzulView as PropertyChangeListener, implemented the possibility to add players to the game via the view to the model.

--- src/azul/team12/view/AzulView.java
+++ src/azul/team12/view/AzulView.java
@@ -1,40 +1,56 @@
 package azul.team12.view;
 
 import azul.team12.controller.Controller;
-import azul.team12.controller.GameController;
 import azul.team12.model.GameModel;
+import azul.team12.model.events.LoginFailedEvent;
 import azul.team12.view.board.GameBoard;
 import azul.team12.view.listeners.TileClickListener;
 import java.awt.CardLayout;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.awt.HeadlessException;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
-public class AzulView extends JFrame {
+public class AzulView extends JFrame implements PropertyChangeListener {
 
   private static final long serialVersionUID = 7526472295622776147L;
 
   private static final String LOGIN_CARD = "login";
-  private static final String HSM_CARD = "hotseatmode";
+  private static final String HOT_SEAT_MODE_CARD = "hotseatmode";
   private static final String NETWORK_CARD = "networkmode";
   private static final String GAMEBOARD_CARD = "gameboard";
   private final GridBagLayout gbl;
   private CardLayout layout;
-  private JTextArea inputNameArea;
-  private JButton hsmButton;
+  private JTextField inputField;
+  private JButton hotSeatModeButton;
   private JButton networkButton;
   private JButton addPlayerButton;
   private JButton playButton;
+  private JLabel numberOfLoggedInPlayersLabel;
   private int numberOfPlayers;
 
   private GameModel model;
+  private Controller controller;
 
+  /**
+   * Create the Graphical User Interface of Azul.
+   *
+   * @param model      the model that handles the logic of the game.
+   * @param controller the controller that is used to communicate with the view.
+   * @throws HeadlessException
+   */
   public AzulView(GameModel model, Controller controller) throws HeadlessException {
+    this.model = model;
+    this.controller = controller;
     setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     gbl = new GridBagLayout();
     setMinimumSize(new Dimension(1400, 900));
@@ -43,14 +59,12 @@ public class AzulView extends JFrame {
     initializeWidgets();
     addEventListeners();
     createView();
-
-    this.model = model;
   }
 
   private void initializeWidgets() {
     layout = new CardLayout();
     //Buttons
-    hsmButton = new JButton("Hot Seat Mode");
+    hotSeatModeButton = new JButton("Hot Seat Mode");
     networkButton = new JButton("Network Mode");
     addPlayerButton = new JButton("+ Add Player");
     playButton = new JButton("Play");
@@ -62,15 +76,59 @@ public class AzulView extends JFrame {
     //TODO: swap lambda expressions with controller functions
     // use the same listener for every Tile (source/destination) object
     TileClickListener tileClickListener = new TileClickListener();
-    hsmButton.addActionListener(event -> showHSMCard());
+    hotSeatModeButton.addActionListener(event -> showHSMCard());
     networkButton.addActionListener(event -> showNetworkCard());
     playButton.addActionListener(event -> showGameBoard(tileClickListener));
-
-    /*
-    addPlayerButton.addActionListener(
-            //TODO: if numberOfPlayers < 5, add another inputNameArea, else disable button
+    addPlayerButton.addActionListener(event -> {
+          controller.addPlayer(inputField.getText());
+          numberOfLoggedInPlayersLabel.setText(
+              "Number of Players: " + (model.getPlayerNamesList().size()) + ".");
+          inputField.setText("");
+        }
     );
-    */
+  }
+
+  @Override
+  public void propertyChange(PropertyChangeEvent event) {
+    SwingUtilities.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        handleModelUpdate(event);
+      }
+    });
+  }
+
+  /**
+   * Handles the events that get fired from the model to the listeners of the model.
+   *
+   * @param event the event that was fired from the model.
+   */
+  private void handleModelUpdate(PropertyChangeEvent event) {
+    Object customMadeGameEvent = event.getNewValue();
+
+    String eventName = event.getPropertyName();
+
+    switch (eventName) {
+      case "LoginFailedEvent" -> {
+        LoginFailedEvent loginFailedEvent = (LoginFailedEvent) customMadeGameEvent;
+        if (loginFailedEvent.getMessage().equals(LoginFailedEvent.NICKNAME_ALREADY_TAKEN)) {
+          showErrorMessage("Nickname is already taken.");
+        } else if (loginFailedEvent.getMessage().equals(LoginFailedEvent.LOBBY_IS_FULL)) {
+          showErrorMessage("Lobby is full.");
+        }
+      }
+      default -> throw new AssertionError("Unknown event");
+    }
+  }
+
+  /**
+   * Show an error message as pop-up window to inform the user of an error.
+   *
+   * @param message the message with information about the error.
+   */
+  private void showErrorMessage(String message) {
+    JOptionPane.showMessageDialog(null, message, "Error!",
+        JOptionPane.ERROR_MESSAGE);
   }
 
   private void createView() {
@@ -80,23 +138,34 @@ public class AzulView extends JFrame {
     JPanel login = new JPanel();
     add(login, LOGIN_CARD);
     login.add(new JLabel("Select Mode"));
-    login.add(hsmButton);
+    login.add(hotSeatModeButton);
     login.add(networkButton);
 
-    JPanel hsmPanel = new JPanel();
-    inputNameArea = new JTextArea();
-    add(hsmPanel, HSM_CARD);
-    hsmPanel.add(new JLabel("Player 1: "));
-    hsmPanel.add(inputNameArea);
-    hsmPanel.add(addPlayerButton);
-    hsmPanel.add(playButton);
+    createHotSeatModeCard();
 
     //controller.startGameBoard
   }
 
+  /**
+   * Adds the Card for the preliminary inputs for the Hot Seat Mode to the card Layout of the view.
+   * This card gets shown if the user selects "hot seat mode" at the start of the program.
+   */
+  private void createHotSeatModeCard() {
+    JPanel hotSeatModePanel = new JPanel();
+    inputField = new JTextField(10);
+    add(hotSeatModePanel, HOT_SEAT_MODE_CARD);
+    numberOfLoggedInPlayersLabel =
+        new JLabel("Number of Players: " + (model.getPlayerNamesList().size()) + ".");
+    hotSeatModePanel.add(numberOfLoggedInPlayersLabel);
+    hotSeatModePanel.add(new JLabel("Please enter your nickname:"));
+    hotSeatModePanel.add(inputField);
+    hotSeatModePanel.add(addPlayerButton);
+    hotSeatModePanel.add(playButton);
+  }
+
   //TODO: add propertyChange function
   private void showHSMCard() {
-    showCard(HSM_CARD);
+    showCard(HOT_SEAT_MODE_CARD);
   }
 
   private void showNetworkCard() {
