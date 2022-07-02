59a227ea57b67f9bdfb492d5857c69afd0211fca
Iurii Irkha <irkhai@cip.ifi.lmu.de>
2022-07-02 17:37:01 +0200

    set transparent background when tile moved.

--- src/azul/team12/view/board/Plate.java
+++ src/azul/team12/view/board/Plate.java
@@ -2,19 +2,14 @@ package azul.team12.view.board;
 
 import azul.team12.model.ModelTile;
 import azul.team12.view.listeners.TileClickListener;
-import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.GridLayout;
-import java.awt.RenderingHints;
-import java.awt.geom.Ellipse2D.Double;
-import java.awt.image.BufferedImage;
+import java.awt.Image;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.LineBorder;
 
 /**
  * Represents a Factory Display on the view.
@@ -32,51 +27,58 @@ public class Plate extends JPanel {
   /**
    * Create one Plate.
    *
-   * @param plateId the number with which the Plate can be identified.
+   * @param plateId           the number with which the Plate can be identified.
    * @param tileClickListener
    */
   public Plate(int plateId,
       TileClickListener tileClickListener, List<ModelTile> content) {
     setLayout(new GridLayout(1, 1));
     JLabel plateImageLabel = new JLabel(
-        getResizedRoundImageIcon("img/manufacturing-plate.png", PLATE_SIZE));
+        getResizedImageIcon("img/manufacturing-plate.png", PLATE_SIZE));
+    setOpaque(false);
     add(plateImageLabel);
     // GridLayout plateLayout = new GridLayout(2, 2);
     plateImageLabel.setBounds(0, 0, PLATE_SIZE, PLATE_SIZE);
     // plateImageLabel.setLayout(plateLayout);
-    setBorder(new LineBorder(Color.WHITE));
     this.plateId = plateId;
     this.tileList = new ArrayList<>();
     int space_from_left = SPACE_FROM_LEFT_AND_TOP_EDGE_IN_PXL;
     int space_from_top = SPACE_FROM_LEFT_AND_TOP_EDGE_IN_PXL;
     for (int i = 0; i < content.size(); i++) {
-          Tile tile = new Tile(i, this.plateId, content.get(i), tileClickListener);
-          tileList.add(tile);
-          tile.setBounds(space_from_left, space_from_top, Tile.TILE_SIZE, Tile.TILE_SIZE);
-          plateImageLabel.add(tile);
-          // move tiles to the right after first and second tile
-          // move down after second tile
-          if (i == 0 || i == 2) {
-            space_from_left += Tile.TILE_SIZE + SPACE_BETWEEN_TILES_IN_PXL;
-          } else if (i == 1) {
-            space_from_left = SPACE_FROM_LEFT_AND_TOP_EDGE_IN_PXL;
-            space_from_top += Tile.TILE_SIZE + SPACE_BETWEEN_TILES_IN_PXL;
-          }
+      Tile tile = new Tile(i, this.plateId, content.get(i), tileClickListener);
+      tileList.add(tile);
+      tile.setBounds(space_from_left, space_from_top, Tile.TILE_SIZE, Tile.TILE_SIZE);
+      plateImageLabel.add(tile);
+      // move tiles to the right after first and second tile
+      // move down after second tile
+      if (i == 0 || i == 2) {
+        space_from_left += Tile.TILE_SIZE + SPACE_BETWEEN_TILES_IN_PXL;
+      } else if (i == 1) {
+        space_from_left = SPACE_FROM_LEFT_AND_TOP_EDGE_IN_PXL;
+        space_from_top += Tile.TILE_SIZE + SPACE_BETWEEN_TILES_IN_PXL;
       }
     }
+  }
 
-  private ImageIcon getResizedRoundImageIcon(String path, int size) {
+//  private ImageIcon getResizedRoundImageIcon(String path, int size) {
+//    URL imgURL1 = getClass().getClassLoader().getResource(path);
+//    ImageIcon icon1 = new ImageIcon(imgURL1);
+//    BufferedImage resizedimage = new BufferedImage(size, size,
+//        BufferedImage.TYPE_INT_RGB);
+//    Graphics2D g2 = resizedimage.createGraphics();
+//    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+//        RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+//    g2.setClip(new Double(0, 0, size, size));
+//    g2.drawImage(icon1.getImage(), 0, 0, size, size, null);
+//
+//    return new ImageIcon(resizedimage);
+//  }
+
+  private ImageIcon getResizedImageIcon(String path, int size) {
     URL imgURL1 = getClass().getClassLoader().getResource(path);
-    ImageIcon icon1 = new ImageIcon(imgURL1);
-    BufferedImage resizedimage = new BufferedImage(size, size,
-        BufferedImage.TYPE_INT_RGB);
-    Graphics2D g2 = resizedimage.createGraphics();
-    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-        RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-    g2.setClip(new Double(0, 0, size, size));
-    g2.drawImage(icon1.getImage(), 0, 0, size, size, null);
-
-    return new ImageIcon(resizedimage);
+    return new ImageIcon(
+        new ImageIcon(imgURL1).getImage()
+            .getScaledInstance(size, size, Image.SCALE_DEFAULT));
   }
 
 }
