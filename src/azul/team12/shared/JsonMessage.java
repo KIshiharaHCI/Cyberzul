9c04934b1b528edf35752f925f1759499ff1ed8c
Nils Sirrenberg <sirrenberg@cip.ifi.lmu.de>
2022-07-08 21:04:53 +0200

    added the posibility to create JSONMessages for every method in the Controller Interface

--- src/azul/team12/shared/JsonMessage.java
+++ src/azul/team12/shared/JsonMessage.java
@@ -1,5 +1,6 @@
 package azul.team12.shared;
 
+import azul.team12.model.Offering;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.Arrays;
@@ -13,7 +14,24 @@ public enum JsonMessage {
 
   LOGIN("login"), LOGIN_SUCCESS("login success"), LOGIN_FAILED("login failed"),
   USER_JOINED("user joined"), POST_MESSAGE("post message"), MESSAGE("message"),
-  USER_LEFT("user left");
+  USER_LEFT("user left"),
+
+  //Controller methods
+  CHOOSE_TILE_FROM("choose tile from"),
+  END_TURN("end turn"),
+  FORFEIT("forfeit"),
+  GET_NICK_OF_ACTIVE_PLAYER("get nick of active player"),
+  GET_NICK_OF_NEXT_PLAYER("get nick of next player"),
+  GET_OFFERINGS("get offerings"),
+  GET_PATTERN_LINES_OF_PLAYER("get pattern lines of player"),
+  GET_PLAYER_NAMES_LIST("get player names list"),
+  GET_POINTS("get points"),
+  GET_TEMPLATE_WALL("get template wall"),
+  GET_WALL_OF_PLAYER_AS_TILES("get wall of player as tiles"),
+  PLACE_TILE_AT_FLOOR_LINE("place tile at floor line"),
+  PLACE_TILE_AT_PATTERN_LINE("place tile at pattern line"),
+  RANKING_PLAYER_WITH_POINTS("ranking player with points"),
+  START_GAME("start game");
 
   public static final String TYPE_FIELD = "type";
 
@@ -24,6 +42,10 @@ public enum JsonMessage {
   public static final String TIME_FIELD = "time";
 
   public static final String ADDITIONAL_INFORMATION = "additional information";
+
+  public static final String INDEX_OF_TILE_FIELD = "index of tile ";
+
+  public static final String INDEX_OF_OFFERING_FIELD = "index of offering";
 
   private final String jsonName;
 
@@ -46,6 +68,52 @@ public enum JsonMessage {
         () -> new IllegalArgumentException(String.format("Unknown message type '%s'", typeName)));
   }
 
+  //Controller methods
+
+  public static JSONObject chooseTileFrom(String playerName, int indexOfTile, int indexOfOffering) {
+    try {
+      JSONObject jsonObject = createMessageOfType(CHOOSE_TILE_FROM);
+      jsonObject.put(INDEX_OF_TILE_FIELD, indexOfTile);
+      jsonObject.put(INDEX_OF_OFFERING_FIELD, indexOfOffering);
+      return jsonObject;
+    } catch (JSONException e) {
+      throw new IllegalArgumentException("Failed to create a json object.", e);
+    }
+  }
+
+  /**
+   * Create a JSONObject by passing only the name of the method.
+   * I.e. "createJSONMessage(END_TURN);
+   *
+   * @param methodName the name of the method in the Controller Interface.
+   * @return a JSONObject containing the information that this specific method was invoked.
+   */
+  public static JSONObject createJSONmessage(JsonMessage methodName) {
+    try {
+      return createMessageOfType(methodName);
+    } catch (JSONException e) {
+      throw new IllegalArgumentException("Failed to create a json object.", e);
+    }
+  }
+
+  /**
+   * Create a JSONObject by passing only the name of the method and the nick of the player.
+   * I.e. "createJSONMessage(GET_POINTS,jonas123);
+   *
+   * @param methodName the name of the method in the Controller Interface.
+   * @param nickname   the nickname of the player that is passed to the method.
+   * @return a JSONObject containing the information that this specific method was invoked with the
+   * name of the player with whom the method was invoked.
+   */
+  public static JSONObject createJSONmessage(JsonMessage methodName, String nickname) {
+    try {
+      return createMessageOfType(methodName).put(NICK_FIELD, nickname);
+    } catch (JSONException e) {
+      throw new IllegalArgumentException("Failed to create a json object.", e);
+    }
+  }
+
+
   public static JSONObject login(String nickname) {
     try {
       return createMessageOfType(LOGIN).put(NICK_FIELD, nickname);
@@ -64,7 +132,7 @@ public enum JsonMessage {
 
   public static JSONObject loginFailed(String reasonForDeniedLogin) {
     try {
-      return createMessageOfType(LOGIN_FAILED).put(ADDITIONAL_INFORMATION,reasonForDeniedLogin);
+      return createMessageOfType(LOGIN_FAILED).put(ADDITIONAL_INFORMATION, reasonForDeniedLogin);
     } catch (JSONException e) {
       throw new IllegalArgumentException("Failed to create a json object.", e);
     }
@@ -122,7 +190,7 @@ public enum JsonMessage {
     }
   }
 
-  public static String getAdditionalInformation(JSONObject object){
+  public static String getAdditionalInformation(JSONObject object) {
     try {
       return object.getString(ADDITIONAL_INFORMATION);
     } catch (JSONException e) {
