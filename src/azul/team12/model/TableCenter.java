9cd6b6c7d0ba49c6c9e425ea3a5a9f9779bca4d0
Marco Zander <marcozander@MacBook-Air-von-Marco.local>
2022-07-12 15:26:23 +0200

    Surpress one warning in initializeContent() method of table center

--- src/azul/team12/model/TableCenter.java
+++ src/azul/team12/model/TableCenter.java
@@ -29,6 +29,9 @@ public class TableCenter extends Offering {
     return instance;
   }
 
+  @edu.umd.cs.findbugs.annotations.SuppressFBWarnings(value = "ST_WRITE_TO_STATIC_FROM"
+      + "_INSTANCE_METHOD", justification = "This is a Singleton, there will be no multiple "
+      + "instances.")
   @Override
   void initializeContent() {
     TableCenter.content = new ArrayList<>();
