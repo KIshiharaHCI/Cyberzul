fecbd445abb92f90b313fa15f38dfefb40e5cb8c
Marco Zander <marcozander@MacBook-Air-von-Marco.local>
2022-07-13 19:48:12 +0200

    Now the changes in the tableCenter class are ok to make and suppressWarnings is possible.

--- src/azul/team12/model/TableCenter.java
+++ src/azul/team12/model/TableCenter.java
@@ -29,9 +29,9 @@ public class TableCenter extends Offering {
     return instance;
   }
 
-  /*@edu.umd.cs.findbugs.annotations.SuppressFBWarnings(value = "ST_WRITE_TO_STATIC_FROM"
+  @edu.umd.cs.findbugs.annotations.SuppressFBWarnings(value = "ST_WRITE_TO_STATIC_FROM"
       + "_INSTANCE_METHOD", justification = "This is a Singleton, there will be no multiple "
-      + "instances.")*/
+      + "instances.")
   @Override
   void initializeContent() {
     TableCenter.content = new ArrayList<>();
