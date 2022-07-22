dde1ef55442f9be9a63ffd7e6edb14dce5917ad9
Marco Zander <marcozander@MacBook-Air-von-Marco.local>
2022-07-22 17:27:08 +0200

    Checkstyles for SinglePlayerPanel

--- src/cyberzul/view/panels/SinglePlayerPanel.java
+++ src/cyberzul/view/panels/SinglePlayerPanel.java
@@ -1,24 +1,27 @@
 package cyberzul.view.panels;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.Serial;
 import java.net.URL;
 import java.util.Objects;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 
-/**
- * Lobby Screen after selecting Single Player Mode
- */
+/** Lobby Screen after selecting Single Player Mode. */
 public class SinglePlayerPanel extends JPanel {
-  private transient BufferedImage image;
-  private Dimension containerDimension;
-  @Serial
-  private static final long serialVersionUID = 17L;
+  @Serial private static final long serialVersionUID = 17L;
+  private final transient BufferedImage image;
+  private final Dimension containerDimension;
 
+  /**
+   * Constructor for the SinglePlayerPanel.
 
+   * @param frameDimension the dimensions for the frame.
+   */
   public SinglePlayerPanel(Dimension frameDimension) {
 
     setMinimumSize(frameDimension);
@@ -26,16 +29,17 @@ public class SinglePlayerPanel extends JPanel {
     setLayout(null);
     setOpaque(false);
 
-    containerDimension = new Dimension((int) (frameDimension.width * 0.7), (int)
-        (frameDimension.height * 0.7));
+    containerDimension =
+        new Dimension((int) (frameDimension.width * 0.7), (int) (frameDimension.height * 0.7));
 
-    JPanel container = new JPanel() {
-      @Override
-      protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.drawImage(image, 0, 0, null);
-        }
-      };
+    JPanel container =
+        new JPanel() {
+          @Override
+          protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(image, 0, 0, null);
+          }
+        };
     container.setBounds(200, 80, containerDimension.width, containerDimension.height);
     container.setOpaque(false);
     add(container);
@@ -43,8 +47,8 @@ public class SinglePlayerPanel extends JPanel {
     try {
       URL imgUrl = getClass().getClassLoader().getResource("img/lobby-panel.png");
       image = ImageIO.read(Objects.requireNonNull(imgUrl));
-      image.getScaledInstance(containerDimension.width, containerDimension.height,
-          Image.SCALE_SMOOTH);
+      image.getScaledInstance(
+          containerDimension.width, containerDimension.height, Image.SCALE_SMOOTH);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
